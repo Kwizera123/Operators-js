0ad1d05acbc4b10d6dd3a38fa9700944e8434ace
Kwizera Theo <kwizerasinapy49@gmail.com>
2023-01-18 18:43:30 +0000

    Added if else statement below operators

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ console.log("--------");
 let c = b / a;
 console.log(c);
 console.log("--------");
+
+//if else statement
+let myapples = 0;
+
+if(myapples === 0){
+myapples += 5;
+console.log("Here you go, now you have", myapples, "apples");
+}else{
+  myapples = 0;
+  console.log("Thank you, You have now",myapples, "apples");
+}
